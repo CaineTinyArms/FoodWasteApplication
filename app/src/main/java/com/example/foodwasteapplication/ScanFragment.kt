4947ff1d9c3e3d6cee9b9addf2576805935faf3c
CaineTinyArms <cainetinyarmsdev@gmail.com>
2025-11-25 13:41:09 +0000

    Added the Kotlin file to be used on the scan screen.

---
+++ app/src/main/java/com/example/foodwasteapplication/ScanFragment.kt
@@ -0,0 +1,22 @@
+package com.example.foodwasteapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ScanFragment : Fragment()
+{
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_scan, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).setTitleText("Scan Item")
+    }
+}
