ca345f95ff9f012b56282ad2b3a0475dfcdca012
CaineTinyArms <146839070+CaineTinyArms@users.noreply.github.com>
2026-01-28 19:36:33 +0000

    Added the Database file

---
+++ app/src/main/java/com/example/foodwasteapplication/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.foodwasteapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [FoodItem::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun foodItemDao(): FoodItemDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "food_waste.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
