b1d0466de1b1c54d825f16ab228dc4ba89a79be4
CaineTinyArms <146839070+CaineTinyArms@users.noreply.github.com>
2026-01-28 21:24:06 +0000

    Updated ListFragment to show the items in the database

--- app/src/main/java/com/example/foodwasteapplication/ListFragment.kt
+++ app/src/main/java/com/example/foodwasteapplication/ListFragment.kt
@@ -1,25 +1,52 @@
 package com.example.foodwasteapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ListFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private val adapter = FoodItemAdapter()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? // used to create the fragment UI from the XML.
-    {
-        return inflater.inflate(R.layout.fragment_list, container, false) // inflater converts XML to View, container is the parent layout.
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    override fun onResume() // when the fragment becomes visible and interactive.
-    {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.foodRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
         super.onResume()
-        (activity as MainActivity).setTitleText("Food List") // cast activity to mainActivity to be able to call the setTitleText Function.
-        (activity as MainActivity).hideSystemUI()
+        (activity as MainActivity).setTitleText("Food List")
+        loadItems()
     }
+
+    private fun loadItems() {
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            val db = AppDatabase.getInstance(requireContext())
+            val items = db.foodItemDao().getAll()
+
+            withContext(Dispatchers.Main) {
+                adapter.submitList(items)
+            }
+        }
+    }
+
 }
-
-
-
